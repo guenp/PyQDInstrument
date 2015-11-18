9ebf656bd3b5783981afa9b03e6c3d09190e7fb3
Guen P <guen@nbi.dk>
2015-11-17 22:09:21 -0500

    server

--- server.py
+++ server.py
@@ -1,17 +1,11 @@
 import clr
 import socket
 
-def run_server(host='192.168.0.103', port=50008, instrument_host='192.168.0.101', instrument_port=11000, verbose=True):
+def run_server(host='', port=50008, instrument_host='192.168.0.101', instrument_port=11000, verbose=True):
 	server = Server(host, port, instrument_host, instrument_port)
 	server.run(verbose)
 
-class Server():
-	def __init__(self, host, port, instrument_host, instrument_port):
-		self.host = host
-		self.port = port
-		self.ins = self.create_instrument(instrument_host,instrument_port)
-
-	def create_instrument(self, host, port):
+def create_instrument(host, port):
 		'''Create remote QDInstrument'''
 		import clr
 		clr.AddReference('QDInstrument')
@@ -20,15 +14,18 @@ class Server():
 		ins = QDInstrument.QDInstrumentFactory.GetQDInstrument(ppms, True, host, port)
 		return ins
 
+class Server():
+	def __init__(self, host, port, instrument_host, instrument_port):
+		self.HOST = host
+		self.PORT = port
+		self.ins = create_instrument(instrument_host,instrument_port)	
+
 	def run(self, verbose=False):
 	    '''Run a measurement server for remote communication.'''
 	    ins = self.ins
-
-	    HOST = self.host                 # Symbolic name meaning all available interfaces
-	    PORT = self.port              # Arbitrary non-privileged port
 	    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-	    s.bind((HOST, PORT))
+	    s.bind((self.HOST, self.PORT))
 	    s.listen(1)
 	    conn, addr = s.accept()
 	    print('Connected by', addr)
