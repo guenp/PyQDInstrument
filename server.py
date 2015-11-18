7ff875f2b0f52660607fb4e9cfe8619a4354586e
Guen P <guen@nbi.dk>
2015-11-18 13:58:18 -0500

    tabs to spaces

--- server.py
+++ server.py
@@ -2,7 +2,7 @@ import socket
 from .pqdinstrument import PPMS
 
 def run_server(host='', port=0, instrument_host='', instrument_port=11000, verbose=True):
-	'''Run a QDInstrument server inside an IronPython console.'''
+    '''Run a QDInstrument server inside an IronPython console.'''
     server = Server(host, port, instrument_host, instrument_port)
     server.run(verbose)
 
@@ -19,25 +19,25 @@ class Server():
         '''Run a measurement server for remote communication.'''
         ppms = self.ppms
         while True:
-        	print('Creating socket at %s:%s...' %(self.HOST, self.PORT))
-	        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-	        s.bind((self.HOST, self.PORT))
-	        s.listen(1)
-	        conn, addr = s.accept()
-	        print('Connected by', addr)
-	        while True:
-	            cmd = conn.recv(1024)
-	            if not cmd: break
-	            try:
-	                cmd = b'ppms.' + cmd
-	                if verbose: print(cmd.decode())
-	                if b'=' in cmd:
-	                    exec(cmd)
-	                    response = 'True'
-	                else:
-	                    response = str(eval(cmd))
-	                conn.sendall(response.encode())
-	            except (SyntaxError, NameError, AttributeError):
-	                conn.sendall(b'Command not recognized.')
-	        conn.close()
+            print('Creating socket at %s:%s...' %(self.HOST, self.PORT))
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            s.bind((self.HOST, self.PORT))
+            s.listen(1)
+            conn, addr = s.accept()
+            print('Connected by', addr)
+            while True:
+                cmd = conn.recv(1024)
+                if not cmd: break
+                try:
+                    cmd = b'ppms.' + cmd
+                    if verbose: print(cmd.decode())
+                    if b'=' in cmd:
+                        exec(cmd)
+                        response = 'True'
+                    else:
+                        response = str(eval(cmd))
+                    conn.sendall(response.encode())
+                except (SyntaxError, NameError, AttributeError):
+                    conn.sendall(b'Command not recognized.')
+            conn.close()
